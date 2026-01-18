7b7d4d80aa9e016ba5345a3db3157986eb634fad
TCSDel <anelkinresale@gmail.com>
2026-01-18 16:38:36 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,3 @@
-import React from "react";
-import { fetchOrders } from "../utils/api";
-
-export default function Home({ orders }) {
-  return (
-    <div>
-      <h1>TCS Delivery Admin Dashboard</h1>
-      <ul>
-        {orders.map((o) => (
-          <li key={o.id}>{o.id} - {o.address}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export async function getServerSideProps() {
-  const orders = await fetchOrders();
-  return { props: { orders } };
+export default function Home() {
+  return <h1>TCS Delivery App is Live</h1>;
 }
